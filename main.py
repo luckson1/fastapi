d69cf896bcd445a74134dbb6f8af11b3ce46180f
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2024-08-25 18:23:41 +0300

    edit template

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 from fastapi import FastAPI
+from langchain_community.document_loaders.youtube import YoutubeLoader, TranscriptFormat
+from fastapi.openapi.utils import get_openapi
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+@app.post("/youtube/")
+def load_youtube_transcript(url: str):
+    loader = YoutubeLoader.from_youtube_url(
+        url,
+        add_video_info=True,
+        transcript_format=TranscriptFormat.CHUNKS,
+        chunk_size_seconds=60,
+    )
+    docs = loader.load()
+    return docs
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Custom title",
+        version="2.5.0",
+        description="This is a very custom OpenAPI schema",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "link": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png",
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
